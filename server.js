d8189df55d4aeed78a0fbba8274fe6066319d8d2
Geoff's Macbook <geoffsmacbook@lawn-143-215-126-8.lawn.gatech.edu>
2018-05-05 10:40:43 -0400

    server.js updates

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
-var characters = [
+// form inputs
+// name
+// phonenumber
+// email
+// uniqueid
+
+var reservations = [
   {
     routeName: "yoda",
     name: "Yoda",
